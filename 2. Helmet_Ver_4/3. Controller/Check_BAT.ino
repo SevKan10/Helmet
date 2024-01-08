edd360fa635e48868b0940fdcbee094168280c26
KKhang <127233909+SevKan10@users.noreply.github.com>
2024-01-08 16:45:03 +0700

    change folder

--- 2. Helmet_Ver_4/3. Controller/Check_BAT.ino
+++
@@ -1,8 +0,0 @@
-void Check_BAT()
-{
-    while(digitalRead(CAG)==1)
-    {
-      for(int i=0; i<=100; i+=10){ drawBattery(i); delay(50); }
-      if(digitalRead(CAG)==0){display.clearDisplay();} 
-    }
-}
